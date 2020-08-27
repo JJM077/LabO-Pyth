a725bb706ad49170c891073b8d4c184b46fab6a1
jjm07 <jjm7410@psu.edu>
2020-08-27 19:37:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# online Python compiler (interpreter) to run Python online 
+# Write Python 3 code in this online editor and run it 
+
+print("Hello World")
+
+name = input("Enter your name: ")
+print("Hello,", name)
+age = int(age)
+print(" You will be", age +1, "next year")
+
